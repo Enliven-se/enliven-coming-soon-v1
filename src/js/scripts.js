0bba527d69ff5bc0d19728808ddac89950cc9076
klokie <github@klokie.com>
2016-01-15 21:04:16 +0100

    reverted to jan 5

---
+++ src/js/scripts.js
@@ -0,0 +1,17 @@
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    if ( ! Modernizr.touch ) {
+      $( '#background' ).YTPlayer();
+    }
+
+    if ( ! Modernizr.svg ) {
+      var $logo = $("#logo");
+      $logo.attr("src", $logo.attr("src").replace(/svg$/, 'png'));
+    }
+
+  });
+
+})(jQuery, window, document);
